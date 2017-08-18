82db69ff7e2e0aba293451fe753d154ef0a1e1e7
Michael Ansel <mansel@box.com>
2017-08-18 16:57:52 -0700

    Refactor getEnergy logic into multiple functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,55 +1,20 @@
 module.exports = {
   getEnergy: function(creep, prioritizeFull=false) {
     var target = Game.getObjectById(creep.memory.target);
-    // TODO optimize search
-    // Find all sources, containers, storage
-    // Filter out all that are empty
-    // Lock on to the closest one
-    // Release lock if it becomes non-viable
-    if (!target) {
-      targets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_CONTAINER ||
-                  structure.structureType == STRUCTURE_STORAGE ) && structure.store[RESOURCE_ENERGY] >= (creep.carryCapacity - creep.carry.energy);
-        }
-      });
-      // if (targets.length == 0) {
-      //   targets = creep.room.find(FIND_STRUCTURES, {
-      //     filter: (structure) => {
-      //       return (structure.structureType == STRUCTURE_CONTAINER ||
-      //               structure.structureType == STRUCTURE_STORAGE ) && structure.store[RESOURCE_ENERGY] >= 0;
-      //     }
-      //   });
-      // }
-      // if (!targets.length) {
-      //   var source = creep.pos.findClosestByPath(FIND_SOURCES);
-      //   if (source) targets.push(source);
-      // }
-      // console.log(creep.name, targets);
-      if (targets.length > 0) {
-        if (prioritizeFull) {
-          target = targets.sort(function(a,b){return a.store[RESOURCE_ENERGY]/a.storeCapacity-b.store[RESOURCE_ENERGY]/b.storeCapacity;}).reverse()[0]; // highest energy
-        } else {
-          target = creep.pos.findClosestByPath(targets);
-        }
-        if (!target) {
-          console.log(creep.name, "Unable to find a path to energy", target, targets);
-          return;
-        }
-      // } else {
-      //   target = creep.pos.findClosestByPath(FIND_SOURCES);
-      //   if (!target) {
-      //     console.log(creep.name, "No available sources");
-      //     return;
-      //   }
-        creep.memory.target = target.id;
+    if (target) {
+      this.getEnergyFromTarget(creep, target);
+    } else {
+      target = this.findAvailableEnergy(creep, prioritizeFull);
+      if (target) {
+        this.getEnergyFromTarget(creep, target);
+      } else {
+        creep.moveTo(Game.flags['RallyWhenLost']);
       }
     }
-    if (!target) {
-      console.log(creep.name, "no energy available");
-      creep.moveTo(Game.flags['RallyWhenLost']);
-      return;
-    }
+  },
+
+  getEnergyFromTarget: function(creep, target) {
+    creep.memory.target = target.id;
 
     var moveToTarget = function() {
       var ret = creep.moveTo(target, {
@@ -73,6 +38,7 @@ module.exports = {
         moveToTarget();
       }
     }
+
     if (target.structureType) {
       if (target.structureType == STRUCTURE_CONTAINER ||
           target.structureType == STRUCTURE_STORAGE) {
@@ -86,5 +52,31 @@ module.exports = {
         }
       }
     }
-  }
+  },
+
+  findAvailableEnergy: function(creep, prioritizeFull) {
+    targets = creep.room.find(FIND_STRUCTURES, {
+      filter: (structure) => {
+        return (structure.structureType == STRUCTURE_CONTAINER ||
+                structure.structureType == STRUCTURE_STORAGE ) && structure.store[RESOURCE_ENERGY] >= (creep.carryCapacity - creep.carry.energy);
+      }
+    });
+    if (targets.length > 0) {
+      if (prioritizeFull) {
+        // select highest energy percentage
+        target = targets.sort(function(a,b){return a.store[RESOURCE_ENERGY]/a.storeCapacity-b.store[RESOURCE_ENERGY]/b.storeCapacity;}).reverse()[0];
+      } else {
+        target = creep.pos.findClosestByPath(targets);
+      }
+      if (!target) {
+        console.log(creep.name, "Unable to find a path to energy", target, targets);
+        return null;
+      }
+    } else {
+      console.log(creep.name, "Unable to find any available energy");
+      return null;
+    }
+
+    return target;
+  },
 };
