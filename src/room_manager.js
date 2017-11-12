92be67221fb6afba6fff8a23f0b546bc98e79ac7
Michael Ansel <mansel@box.com>
2017-11-12 10:57:29 -0800

    Spawn minhauler after miner

--- src/room_manager.js
+++ src/room_manager.js
@@ -131,7 +131,7 @@ const RoomManager = {
       return mineral && mineral.mineralAmount > 0;
     });
     room.memory.desiredCreepCounts.miner = extractors.length;
-    room.memory.desiredCreepCounts.minhauler = Math.min(1,extractors.length);
+    room.memory.desiredCreepCounts.minhauler = Math.min(1,helpers.creepsInRoomWithRole(room, 'miner').length);
 
     if(!room.memory.roomsToClaim) room.memory.roomsToClaim = [];
     room.memory.roomsToClaim = room.memory.roomsToClaim.filter(function(rn){return !(Game.rooms[rn] && Game.rooms[rn].controller.my);});
