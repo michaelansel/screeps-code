9da9856d8c48137ea27a493a7a312b6e77bc7186
Michael Ansel <git@anselcomputers.com>
2022-03-01 05:06:40 +0000

    Add logging framework

--- src/utils/Console.ts
+++ src/utils/Console.ts
@@ -1,6 +1,8 @@
 import { SourcePlanner } from "planners/SourcePlanner";
+import { Logger } from "./Logger";
 
 export const Console = {
+    Logger: () => { return Logger.instance; },
     SourcePlanner: () => { return SourcePlanner.instance; },
     resetSourcePlanner: () => {
         Object.values(Game.creeps).forEach((creep) => { creep.memory.source = undefined }); delete Memory.SourcePlanner?.creeps;
---
+++ test/unit/utils/Logger.test.ts
@@ -0,0 +1,44 @@
+const sinon = require('sinon');
+import { assert } from "sinon";
+import { Logger, DEFAULT_COMPONENT } from "utils/Logger";
+
+describe("Logger", () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it("should output when all logging is enabled", () => {
+        Logger.instance.setComponentLogLevel(DEFAULT_COMPONENT, "DEBUG");
+        const consoleLog = sinon.fake();
+        Logger.instance.setOutputFunction(consoleLog);
+        Logger.instance.debug("test");
+        assert.calledOnce(consoleLog);
+    });
+
+    it("should output if component is enabled", () => {
+        Logger.instance.setComponentLogLevel(DEFAULT_COMPONENT, "ERROR");
+        Logger.instance.setComponentLogLevel("test", "DEBUG");
+        const consoleLog = sinon.fake();
+        Logger.instance.setOutputFunction(consoleLog);;
+        Logger.get("test").debug("test");
+        assert.calledOnce(consoleLog);
+    });
+
+    it("should output if parent component is enabled", () => {
+        Logger.instance.setComponentLogLevel(DEFAULT_COMPONENT, "DEBUG");
+        Logger.instance.setComponentLogLevel("test", "ERROR");
+        const consoleLog = sinon.fake();
+        Logger.instance.setOutputFunction(consoleLog);
+        Logger.get("test").debug("test");
+        assert.calledOnce(consoleLog);
+    });
+
+    it("should not output if all components are disabled", () => {
+        Logger.instance.setComponentLogLevel(DEFAULT_COMPONENT, "ERROR");
+        Logger.instance.setComponentLogLevel("test", "ERROR");
+        const consoleLog = sinon.fake();
+        Logger.instance.setOutputFunction(consoleLog);
+        Logger.get("test").debug("test");
+        assert.notCalled(consoleLog);
+    });
+});
