5c6edc61d9ffe7d6623c6ef7c72db98eb3c7109f
Michael Ansel <git@anselcomputers.com>
2024-09-17 13:27:25 +0000

    Align with new Task typing

--- src/utils/Console.ts
+++ src/utils/Console.ts
@@ -1,10 +1,11 @@
 import { SourcePlanner } from "planners/SourcePlanner";
 import { Logger } from "./Logger";
+import { HarvestEnergyTask } from "tasks";
 
 export const Console = {
     Logger: () => { return Logger.instance; },
     SourcePlanner: () => { return SourcePlanner.instance; },
     resetSourcePlanner: () => {
-        Object.values(Game.creeps).forEach((creep) => { creep.memory.source = undefined }); delete Memory.SourcePlanner?.creeps;
+        Object.values(Game.creeps).forEach((creep) => { if (creep.task == HarvestEnergyTask) creep.stopTask() }); delete Memory.SourcePlanner?.creeps;
     },
 };
