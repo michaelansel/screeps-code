c28b1c73eda4596de44d46c95ad4dc87b4d635ae
Michael Ansel <git@anselcomputers.com>
2024-09-18 05:18:42 +0000

    Add helper function for displaying all logs

--- src/utils/Console.ts
+++ src/utils/Console.ts
@@ -4,6 +4,7 @@ import { HarvestEnergyTask } from "tasks";
 
 export const Console = {
     Logger: () => { return Logger.instance; },
+    LogEverything: () => { Console.Logger().setComponentLogLevel("DEFAULT_COMPONENT", "DEBUG"); },
     SourcePlanner: () => { return SourcePlanner.instance; },
     resetSourcePlanner: () => {
         Object.values(Game.creeps).forEach((creep) => { if (creep.task == HarvestEnergyTask) creep.stopTask() }); delete Memory.SourcePlanner?.creeps;
